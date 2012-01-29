7f2b9d4e22dfbfc8c4198e2cca2ec187d1da11a8
rjaffari@gmail.com <rjaffari@gmail.com@3195375d-7cfc-d6e3-db11-b98cc0a75730>
2012-01-29 18:43:43 +0000

    Added logic to read by tag name.

--- src/com/Team3/Tardis/XML/PeopleReader.java
+++ src/com/Team3/Tardis/XML/PeopleReader.java
@@ -2,12 +2,15 @@ package com.Team3.Tardis.XML;
 
 import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.Iterator;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 
+import org.apache.commons.jxpath.JXPathContext;
+import org.apache.commons.jxpath.Pointer;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
@@ -73,6 +76,59 @@ public class PeopleReader implements IPeopleReader {
 		Logger.log("loadPeople() - END ");
 		return people;
 	}
+	
+	// new logic to read start
+	public ArrayList<Person> loadPeople_NEW_LOGIC(String path) {
+
+		Logger.log("loadPeople() - START ");
+		Logger.log("docPath = " + path);
+		ArrayList<Person> people = new ArrayList<Person>();
+		InputStream is = null;
+
+		try {
+			is = new FileInputStream(path);
+			Logger.log("is = " + is);
+			
+			JXPathContext ctx = XPathHelper.getDocumentContext(is);
+
+			Iterator<Pointer> peopleIt = ctx.iteratePointers("/people/person");
+			while(peopleIt.hasNext()){
+
+				Person person = new Person();
+				Pointer personPtr = peopleIt.next();
+				JXPathContext personCtx = ctx.getRelativeContext(personPtr);
+				Object id = personCtx.getValue("id");
+				Object firstName = personCtx.getValue("firstName");
+				Object lastName = personCtx.getValue("lastName");
+				
+				person.setPersonId(Integer.parseInt((id == null)?"-1":(String)id));
+				person.setFirstName(((firstName == null)?"":(String)firstName));
+				person.setLastName(((lastName == null)?"":(String)lastName));
+				
+				Logger.log(person.getPersonId() + " : " + person.getFirstName() + " " + person.getLastName());
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			Logger.log("FileNotFoundException " + e.getMessage());
+		}
+		finally {
+			if(is != null) {
+				try {
+					is.close();
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+					Logger.log(" MalformedURLException " + e.getMessage());
+				} catch (IOException e) {
+					e.printStackTrace();
+					Logger.log("IOException " + e.getMessage());
+				}
+			}
+		}
+
+		Logger.log("loadPeople() - END ");
+		return people;
+	}
+	// new logic to read end
 
 	private Person loadPerson(Node personNode) {
 
