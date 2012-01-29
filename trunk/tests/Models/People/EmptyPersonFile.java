617b1a6573e4c32d2920899a15c1b8557b1bad95
alex@landovskis.com <alex@landovskis.com@3195375d-7cfc-d6e3-db11-b98cc0a75730>
2012-01-29 17:19:53 +0000

    Added unit tests.

---
+++ trunk/tests/Models/People/EmptyPersonFile.java
@@ -0,0 +1,22 @@
+package com.Team3.Tardis.Tests.Models.People;
+
+import org.junit.Before;
+
+/**
+ * 
+ */
+
+/**
+ * @author j_landov, 
+ *
+ */
+public class PersonTest extends {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+}
---
+++ trunk/tests/Models/People/PeopleLoad.java
@@ -0,0 +1,17 @@
+package com.Team3.Tardis.Tests.Models.People;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+
+public class PeopleLoad {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+}
