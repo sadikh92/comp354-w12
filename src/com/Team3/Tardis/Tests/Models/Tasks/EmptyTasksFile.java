361262c2fda43fb8e70a97c3c0fb93982f18a4eb
sadikhl89n@gmail.com <sadikhl89n@gmail.com@3195375d-7cfc-d6e3-db11-b98cc0a75730>
2012-02-03 03:34:02 +0000

    tested the taskReader for an empty file

--- src/com/Team3/Tardis/Tests/Models/Tasks/EmptyTasksFile.java
+++ src/com/Team3/Tardis/Tests/Models/Tasks/EmptyTasksFile.java
@@ -1,10 +1,22 @@
 package com.Team3.Tardis.Tests.Models.Tasks;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import com.Team3.Tardis.Models.Person;
+import com.Team3.Tardis.Models.Task;
+import com.Team3.Tardis.XML.InputValidator;
+import com.Team3.Tardis.XML.PeopleReader;
+import com.Team3.Tardis.XML.TaskReader;
+
 
 public class EmptyTasksFile {
+	static final String EMPTY_TASK_FILE = "xml/empty_task.xml";
+	static final String NO_TASK_FILE = "xml/empty_task_xml_tags.xml";
 
 	@Before
 	public void setUp() throws Exception {
@@ -13,6 +25,14 @@ public class EmptyTasksFile {
 	@Test
 	public void test()
 	{
-		
+		TaskReader reader = new TaskReader(new InputValidator());
+		ArrayList<Task> taskList = null;
+		try {
+			taskList = reader.loadTasks(EMPTY_TASK_FILE);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals(0, taskList.size());
 	}
 }
