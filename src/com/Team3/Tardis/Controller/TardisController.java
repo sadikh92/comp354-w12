a5ef3254ffa261d069b4e50a47b0f69106cf4577
rjaffari@gmail.com <rjaffari@gmail.com@3195375d-7cfc-d6e3-db11-b98cc0a75730>
2012-02-03 03:02:56 +0000

    Extended logger to output the active class name.

--- src/com/Team3/Tardis/Controller/TardisController.java
+++ src/com/Team3/Tardis/Controller/TardisController.java
@@ -1,12 +1,6 @@
 package com.Team3.Tardis.Controller;
 
-import static org.junit.Assert.assertEquals;
-
 import java.util.ArrayList;
-
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
 
 import com.Team3.Tardis.Models.Person;
 import com.Team3.Tardis.XML.InputValidator;
@@ -20,21 +14,15 @@ public class TardisController {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		InputValidator validator = new InputValidator();
 		PeopleReader reader = new PeopleReader(validator);
-		ArrayList<Person> people = null;
+		ArrayList<Person> people;
 		
 		try {
 			people = reader.loadPeople(PEOPLE_FILE);
-			
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			Logger.log(e.toString());
-			//e.printStackTrace();
-		}
-		
+			Logger.log(TardisController.class.getName(), e.toString());
+		}		
 	}
-
 }
