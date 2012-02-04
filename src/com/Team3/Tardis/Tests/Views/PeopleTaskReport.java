2d49fe9407ff19d7776e0a793c66228e9fc97f28
alex@landovskis.com <alex@landovskis.com@3195375d-7cfc-d6e3-db11-b98cc0a75730>
2012-02-04 20:01:54 +0000

    Added code to test the people task table. Added a test suite that runs all tests.

--- src/com/Team3/Tardis/Tests/Views/PeopleTaskReport.java
+++ src/com/Team3/Tardis/Tests/Views/PeopleTaskReport.java
@@ -1,6 +1,6 @@
 package com.Team3.Tardis.Tests.Views;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.io.*;
@@ -17,18 +17,10 @@ import com.Team3.Tardis.logger.Logger;
 
 public class PeopleTaskReport 
 {
-	static final String PEOPLE_FILE = "xml/people.xml";
 	static final String ONE_TASK_TEST_FILE = "report/report_test.txt";
-	static final String ONE_TASK_ACTUAL_FILE = "report/report.txt";
+	static final String NO_TASK_TEST_FILE = "report/report_unassigned.txt";
+	static final String REPORT_FILE = "report/report.txt";
 	
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
 	@Test
 	/*
 	 * @description Everyone is assigned at least one task.
@@ -45,11 +37,12 @@ public class PeopleTaskReport
 			BufferedReader testReader = new BufferedReader(testInputStream);
 			
 			// Actual report file.
-			FileInputStream reportFileStream = new FileInputStream(ONE_TASK_ACTUAL_FILE);
+			FileInputStream reportFileStream = new FileInputStream(REPORT_FILE);
 			InputStreamReader reportInputStream = new InputStreamReader(reportFileStream);
 			BufferedReader reportReader = new BufferedReader(reportInputStream);
 			
-			// Read the file line by line.
+			// Read the file line by line. 
+			// Every line could be equal to the corresponding line in the other file.
 			String testLine = testReader.readLine();
 			String reportLine = reportReader.readLine();
 			while (testLine != null && reportLine != null)
@@ -58,6 +51,9 @@ public class PeopleTaskReport
 				testLine = testReader.readLine();
 				reportLine = reportReader.readLine();
 			}
+			// Both files must have the same number of lines.
+			assertNull(REPORT_FILE + " has more lines than " + ONE_TASK_TEST_FILE, reportLine);
+			assertNull(ONE_TASK_TEST_FILE + " has more lines than " + REPORT_FILE, testLine);
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -66,16 +62,48 @@ public class PeopleTaskReport
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
 	}
 	
 	@Test
 	/*
-	 * @description Some people do have a task.
+	 * @description Some people have tasks, others do not. Some tasks are unassigned.
 	 */
 	public void testNoTask()
 	{
-		
+		try {
+			// Report Generation
+			// PeopleTaskReport.report().
+			
+			// Test file.
+			FileInputStream testFileStream = new FileInputStream(NO_TASK_TEST_FILE);
+			InputStreamReader testInputStream = new InputStreamReader(testFileStream);
+			BufferedReader testReader = new BufferedReader(testInputStream);
+			
+			// Actual report file.
+			FileInputStream reportFileStream = new FileInputStream(REPORT_FILE);
+			InputStreamReader reportInputStream = new InputStreamReader(reportFileStream);
+			BufferedReader reportReader = new BufferedReader(reportInputStream);
+			
+			// Read the file line by line. 
+			// Every line could be equal to the corresponding line in the other file.
+			String testLine = testReader.readLine();
+			String reportLine = reportReader.readLine();
+			while (testLine != null && reportLine != null)
+			{
+				assertEquals(testLine, reportLine);
+				testLine = testReader.readLine();
+				reportLine = reportReader.readLine();
+			}
+			// Both files must have the same number of lines.
+			assertNull(REPORT_FILE + " has more lines than " + NO_TASK_TEST_FILE, reportLine);
+			assertNull(NO_TASK_TEST_FILE + " has more lines than " + REPORT_FILE, testLine);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
