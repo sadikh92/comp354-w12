cf15cea86d8bd846c2b6267d04d19ec7289be5b3
edana161 <edana161@3195375d-7cfc-d6e3-db11-b98cc0a75730>
2012-03-04 18:17:33 +0000

    File menu added. Placeholders for save and print functions right now

--- trunk/src/com/Team3/Tardis/Views/TardisShell.java
+++ trunk/src/com/Team3/Tardis/Views/TardisShell.java
@@ -4,12 +4,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.BorderLayout;
 import java.util.ArrayList;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.event.ChangeEvent;
@@ -33,6 +34,20 @@ private ArrayList<Person> people;
     this.tasks = tasks;
 	this.people = people;
     
+//creation of file menu	
+	JMenuBar menuBar = new JMenuBar();
+	setJMenuBar(menuBar);
+	JMenu fileMenu = new JMenu("File");
+	menuBar.add(fileMenu);
+	
+	
+	JMenuItem save = new JMenuItem("Save");
+	JMenuItem printSubMenu = new JMenu("Print");
+	JMenuItem exit = new JMenuItem("Exit");
+	JMenuItem printTask = new JMenuItem("Print Tasks");
+	JMenuItem printPeople = new JMenuItem("Print People");
+	
+	
 //creation of tabs    
     TaskUI taskPanel = new TaskUI(this.tasks, this.people);
     JLabel taskLabel = new JLabel();
@@ -51,54 +66,60 @@ private ArrayList<Person> people;
     JLabel ganttLabel = new JLabel();
     ganttLabel.setText("GANTT");
     
-//exit button listeners    
-    JButton exitButton1 = new JButton("Exit");
-    exitButton1.addActionListener(new ActionListener(){
-    	public void actionPerformed(ActionEvent e){
-    		System.exit(0);
-    	}
-    });
+	fileMenu.add(save);
+	fileMenu.add(printSubMenu);
+	fileMenu.add(exit);
+	printSubMenu.add(printTask);
+	printSubMenu.add(printPeople);
     
-    JButton exitButton2 = new JButton("Exit");
-    exitButton1.addActionListener(new ActionListener(){
-    	public void actionPerformed(ActionEvent e){
-    		System.exit(0);
-    	}
-    });
-    
-    JButton exitButton3 = new JButton("Exit");
-    exitButton1.addActionListener(new ActionListener(){
-    	public void actionPerformed(ActionEvent e){
-    		System.exit(0);
-    	}
-    });
-    
-    JButton exitButton4 = new JButton("Exit");
-    exitButton1.addActionListener(new ActionListener(){
-    	public void actionPerformed(ActionEvent e){
-    		System.exit(0);
-    	}
-    });
-    
-//adding tabs to pane
+//adding tabs to frame
     tabbedPane.addTab("Tasks", taskPanel);
     tabbedPane.addTab("People", peoplePanel);
     tabbedPane.addTab("Tree", treePanel);
     tabbedPane.addTab("GANTT", ganttPanel);
     
-//adding exit buttons
-    taskPanel.add(exitButton1, BorderLayout.PAGE_END);
-    peoplePanel.add(exitButton2, BorderLayout.PAGE_END);
-    treePanel.add(exitButton3, BorderLayout.PAGE_END);
-    ganttPanel.add(exitButton4, BorderLayout.PAGE_END);
-    
-   
+
+//action listener for the save function
+	save.addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent e)
+		{
+			System.out.println("Save function here");
+		}
+	});
+	
+//action listener for the exit function	
+	exit.addActionListener(new ActionListener(){
+	  public void actionPerformed(ActionEvent e)
+	  {
+		  System.exit(0);
+	  }
+	}); 
+	
+//print action listener
+	printTask.addActionListener(new ActionListener(){
+	  public void actionPerformed(ActionEvent e)
+	  {
+		  System.out.println("Print tasks here");
+	  }
+	}); 
+
+	printPeople.addActionListener(new ActionListener(){
+	  public void actionPerformed(ActionEvent e)
+	 {
+		  System.out.println("Print People here");
+	 }
+   }); 
+	
+	
 //change listener for tabs    
     tabbedPane.addChangeListener(new ChangeListener() {
-      public void stateChanged(ChangeEvent e) {
-        tabbedPane.revalidate();
+      public void stateChanged(ChangeEvent e) 
+      {
+    	  tabbedPane.revalidate();
       }
     });
+   
+    
     getContentPane().add(tabbedPane);
   }
   
