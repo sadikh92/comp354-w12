46767d342b083f6f5e964da782cc39804ce6dce3
edana161 <edana161@3195375d-7cfc-d6e3-db11-b98cc0a75730>
2012-03-04 15:29:17 +0000

    Made everything (slightly) less sucky

--- src/com/Team3/Tardis/ShellUI/tardisShell.java
+++ src/com/Team3/Tardis/ShellUI/tardisShell.java
@@ -1,10 +1,9 @@
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -12,23 +11,74 @@ import javax.swing.JTabbedPane;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+
 public class tardisShell extends JFrame {
 
-  String[] titles = { "Tasks", "People", "Tree", "GANTT"};
-
-  JTabbedPane tabbedPane;
-  
+JTabbedPane tabbedPane = new JTabbedPane();
   
   public tardisShell() {
     super("TARDIS Task Manager");
-    tabbedPane = new JTabbedPane() {
-
-    };
-    for (int i = 0; i < titles.length; i++) {
-      tabbedPane.addTab(titles[i], createPane(titles[i]));
-    }
-    tabbedPane.setSelectedIndex(0);
-
+    
+    tabbedPane = new JTabbedPane();
+    
+    JPanel taskPanel = new JPanel();
+    JLabel taskLabel = new JLabel();
+    taskLabel.setText("Tasks");
+    
+    JPanel peoplePanel = new JPanel();
+    JLabel peopleLabel = new JLabel();
+    peopleLabel.setText("People");
+    
+    JPanel treePanel = new JPanel();
+    JLabel treeLabel = new JLabel();
+    treeLabel.setText("Tree");
+    
+    JPanel ganttPanel = new JPanel();
+    JLabel ganttLabel = new JLabel();
+    ganttLabel.setText("GANTT");
+    
+    
+    JButton exitButton1 = new JButton("Exit");
+    exitButton1.addActionListener(new ActionListener(){
+    	public void actionPerformed(ActionEvent e){
+    		System.exit(0);
+    	}
+    });
+    
+    JButton exitButton2 = new JButton("Exit");
+    exitButton1.addActionListener(new ActionListener(){
+    	public void actionPerformed(ActionEvent e){
+    		System.exit(0);
+    	}
+    });
+    
+    JButton exitButton3 = new JButton("Exit");
+    exitButton1.addActionListener(new ActionListener(){
+    	public void actionPerformed(ActionEvent e){
+    		System.exit(0);
+    	}
+    });
+    
+    JButton exitButton4 = new JButton("Exit");
+    exitButton1.addActionListener(new ActionListener(){
+    	public void actionPerformed(ActionEvent e){
+    		System.exit(0);
+    	}
+    });
+    
+    
+    tabbedPane.addTab("Tasks", taskPanel);
+    tabbedPane.addTab("People", peoplePanel);
+    tabbedPane.addTab("Tree", treePanel);
+    tabbedPane.addTab("GANTT", ganttPanel);
+    
+    taskPanel.add(exitButton1);
+    peoplePanel.add(exitButton2);
+    treePanel.add(exitButton3);
+    ganttPanel.add(exitButton4);
+    
+   
+    
     tabbedPane.addChangeListener(new ChangeListener() {
       public void stateChanged(ChangeEvent e) {
         tabbedPane.revalidate();
@@ -38,32 +88,15 @@ public class tardisShell extends JFrame {
   }
 
   
-  JPanel createPane(String title) {
-    JPanel panel = new JPanel();
-    JLabel label = new JLabel(title);
-    JButton quitButton = new JButton("Quit");
-    quitButton.setBounds(50, 60, 80, 30);
-    quitButton.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent event) {
-            System.exit(0);
-       }
-    });
-    label.setOpaque(true);
-    panel.add(label);
-    panel.add(quitButton);
-    return panel;
-  }
-
-
-  
   public static void main(String[] args) {
+
     JFrame frame = new tardisShell();
     frame.addWindowListener(new WindowAdapter() {
       public void windowClosing(WindowEvent e) {
         System.exit(0);
       }
     });
-    frame.setSize(360, 100);
+    frame.setSize(400, 400);
     frame.setVisible(true);
   }
 }
