7c68ad2244155d9a1f61170cc8f7ff9cc24aee4b
constablebumley@yahoo.ca <constablebumley@yahoo.ca@3195375d-7cfc-d6e3-db11-b98cc0a75730>
2012-03-05 19:11:43 +0000

    Cleaned up the code, added comments, added pop up confirmation windows.

--- trunk/src/com/Team3/Tardis/Views/TardisShell.java
+++ trunk/src/com/Team3/Tardis/Views/TardisShell.java
@@ -7,10 +7,10 @@ import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.event.ChangeEvent;
@@ -20,147 +20,194 @@ import com.Team3.Tardis.Models.Person;
 import com.Team3.Tardis.Models.Task;
 
 public class TardisShell extends JFrame {
+	//Holds the tabbed pane
+	JTabbedPane tabbedPane = new JTabbedPane();
 
-JTabbedPane tabbedPane = new JTabbedPane();
-
-//initializing task and person array lists
-private ArrayList<Task> tasks;
-private ArrayList<Person> people;
-  
-private TaskUI taskPanel;
-private PeopleUI peoplePanel;
-
-
-//constructor
-  public TardisShell(ArrayList<Task> tasks, ArrayList<Person> people) {
-    super("TARDIS Task Manager");
-    
-    this.tasks = tasks;
-	this.people = people;
-    
-//creation of file menu	
-	JMenuBar menuBar = new JMenuBar();
-	setJMenuBar(menuBar);
-	JMenu fileMenu = new JMenu("File");
-	menuBar.add(fileMenu);
-
-//menu items	
-	JMenuItem save = new JMenuItem("Save");
-	JMenuItem printSubMenu = new JMenu("Print");
-	JMenuItem exit = new JMenuItem("Exit");
-	JMenuItem printTask = new JMenuItem("Print Tasks");
-	JMenuItem printPeople = new JMenuItem("Print People");
+	//Holds the array of tasks and the array of people
+	private ArrayList<Task> tasks;
+	private ArrayList<Person> people;
+	 
+	//Stores the panels created by TaskUI and PeopleUI
+	private TaskUI taskPanel;
+	private PeopleUI peoplePanel;
 	
+	//constructor
+	public TardisShell(ArrayList<Task> tasks, ArrayList<Person> people) {
+		super("TARDIS Task Manager");
+	    
+	    //Sets tasks and people with a shallow copy so that changes affect the arrayLists
+	    //in the main
+	    this.tasks = tasks;
+		this.people = people;
+	    
+		//Creates the file menu	
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		JMenu fileMenu = new JMenu("File");
+		menuBar.add(fileMenu);
 	
-//creation of tabs    
-    taskPanel = new TaskUI(this, this.tasks, this.people);
-    JLabel taskLabel = new JLabel();
-    taskLabel.setText("Tasks");
-    
-    peoplePanel = new PeopleUI(this, this.tasks, this.people);
-    JLabel peopleLabel = new JLabel();
-    peopleLabel.setText("People");
-    
-    JPanel treePanel = new JPanel();
-    JLabel treeLabel = new JLabel();
-    treeLabel.setText("Tree");
-    
-    JPanel ganttPanel = new JPanel();
-    JLabel ganttLabel = new JLabel();
-    ganttLabel.setText("GANTT");
-    
-	fileMenu.add(save);
-	fileMenu.add(printSubMenu);
-	fileMenu.add(exit);
-	printSubMenu.add(printTask);
-	printSubMenu.add(printPeople);
-    
-//adding tabs to frame
-    tabbedPane.addTab("Tasks", taskPanel);
-    tabbedPane.addTab("People", peoplePanel);
-    tabbedPane.addTab("Tree", treePanel);
-    tabbedPane.addTab("GANTT", ganttPanel);
-    
+		//Creates the menu items	
+		JMenuItem save = new JMenuItem("Save");
+		JMenuItem printSubMenu = new JMenu("Print");
+		JMenuItem exit = new JMenuItem("Exit");
+		JMenuItem printTask = new JMenuItem("Print Tasks");
+		JMenuItem printPeople = new JMenuItem("Print People");
+		
+		//Adds them to the menu
+		fileMenu.add(save);
+		fileMenu.add(printSubMenu);
+		fileMenu.add(exit);
+		printSubMenu.add(printTask);
+		printSubMenu.add(printPeople);
+		
+		//Creates the task panel 
+	    taskPanel = new TaskUI(this, this.tasks, this.people);
+	    
+	    //Creates the people panel
+	    peoplePanel = new PeopleUI(this, this.tasks, this.people);
+	    
+	    //Creates the tree panel
+	    JPanel treePanel = new JPanel();
+	    
+	    //Creates the GANTT panel
+	    JPanel ganttPanel = new JPanel();
+	    
+	    //Adds a tab to the frame for each panel
+	    tabbedPane.addTab("Tasks", taskPanel);
+	    tabbedPane.addTab("People", peoplePanel);
+	    tabbedPane.addTab("Tree", treePanel);
+	    tabbedPane.addTab("GANTT", ganttPanel);
+	    
+	    //This is what was written before
+	    //getContentPane().add(tabbedPane);
+	    //But I believe that this is equivalent since we're in a frame
+	    add(tabbedPane);
+	    
+	    //Action listener for the save function
+		save.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e)
+			{
+				//*************************
+				//Call to the save function
+				//*************************
+				
+				JOptionPane.showMessageDialog(null, "Your changes have been saved to the XML files.");
+			}
+		});
+		
+		//Action listener for the exit function	
+		exit.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e)
+			{
+				//Asks the user whether or not they want to save before quitting
+				int n = JOptionPane.showConfirmDialog(null,
+						"Would you like to save your changes before exiting?",
+						"Save Confirmation", JOptionPane.YES_NO_OPTION);
 
-//action listener for the save function
-	save.addActionListener(new ActionListener(){
-		public void actionPerformed(ActionEvent e)
-		{
-			//Save function
-		}
-	});
+				if (n == JOptionPane.YES_OPTION)
+				{
+					//*************************
+					//Call to the save function
+					//*************************
+				}
+				else if (n == JOptionPane.NO_OPTION)
+				{
+					//Nothing happens
+				}
+				else
+				{
+					System.out.println("Unexpected error.");
+				}
+				
+				System.exit(0);
+			}
+		}); 
+		
+		//Action listener for the task printer	
+		printTask.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e)
+			{
+				taskPrinter();
+				
+				JOptionPane.showMessageDialog(null, "The Table View of Tasks has been printed to a text file.");
+			}
+		}); 
 	
-//action listener for the exit function	
-	exit.addActionListener(new ActionListener(){
-	  public void actionPerformed(ActionEvent e)
-	  {
-		  //Save function
-		  System.exit(0);
-	  }
-	}); 
-	
-//printing action listeners
-	printTask.addActionListener(new ActionListener(){
-	  public void actionPerformed(ActionEvent e)
-	  {
-		  //Save Function
-		  taskPrinter();
-		  System.out.println("Print tasks here");
-	  }
-	}); 
-
-	printPeople.addActionListener(new ActionListener(){
-	  public void actionPerformed(ActionEvent e)
-	 {
-		  //Save Function
-		  peoplePrinter();
-		  System.out.println("People list printed");
-	 }
-   }); 
-	
-//change listener for tabs    
-    tabbedPane.addChangeListener(new ChangeListener() {
-      public void stateChanged(ChangeEvent e) 
-      {
-    	  tabbedPane.revalidate();
-      }
-    });
-   
-    
-    getContentPane().add(tabbedPane);
-  }
-  
-  public void update() {
-	  taskPanel.update();
-	  peoplePanel.update();
-  }
-  
-  public void taskPrinter(){
-	  TaskView taskView = new TaskView();
-	  taskView.view("Table View of Tasks.txt", this.people, this.tasks);
-  }
-  
-  public void peoplePrinter(){
-	  PeopleView peopleView = new PeopleView();
-	  peopleView.view("Table View of People.txt", this.people, this.tasks);
+		//Action listener for the task printer	
+		printPeople.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e)
+			 {
+				peoplePrinter();
+				
+				JOptionPane.showMessageDialog(null, "The Table View of People has been printed to a text file.");
+			 }
+	   }); 
+		
+		//Change listener for tabs    
+	    tabbedPane.addChangeListener(new ChangeListener() {
+	    	public void stateChanged(ChangeEvent e) 
+	    	{
+	    		tabbedPane.revalidate();
+	    	}
+	    });
 	}
-	
-	
-  
-//create and show
-  public static void createAndShowGUI(ArrayList<Task> tasks, ArrayList<Person> people)
-	{
+	  
+	//Update will be called to update the different panels when a change is made
+	//to the Table View of Tasks
+	public void update() {
+		taskPanel.update();
+		peoplePanel.update();
+	}
+	  
+	//Prints a text file of the Table View of Tasks
+	public void taskPrinter(){
+		TaskView taskView = new TaskView();
+		taskView.view("Table View of Tasks.txt", this.people, this.tasks);
+	}
+	  
+	//Prints a text file of the Table View of People
+	public void peoplePrinter(){
+		PeopleView peopleView = new PeopleView();
+		peopleView.view("Table View of People.txt", this.people, this.tasks);
+	}
+		
+	//Static Method used to create the Frame
+	//Called by the main
+	public static void createAndShowGUI(ArrayList<Task> tasks, ArrayList<Person> people) {
+		//Creates a new frame
 		JFrame frame = new TardisShell(tasks, people);
 		
-	    frame.addWindowListener(new WindowAdapter()
+		//Listens for the closing of the window
+		frame.addWindowListener(new WindowAdapter()
 		{
 			public void windowClosing(WindowEvent e)
 			{
+				//Asks the user whether or not they want to save before quitting
+				int n = JOptionPane.showConfirmDialog(null,
+						"Would you like to save your changes before exiting?",
+						"Save Confirmation", JOptionPane.YES_NO_OPTION);
+
+				if (n == JOptionPane.YES_OPTION)
+				{
+					//*************************
+					//Call to the save function
+					//*************************
+				}
+				else if (n == JOptionPane.NO_OPTION)
+				{
+					//Nothing happens
+				}
+				else
+				{
+					System.out.println("Unexpected error.");
+				}
+				
 				System.exit(0);
 			}
 		});
-			
-		frame.setSize(1200, 600);
+		
+		//Displays the frame of the right size
+		frame.setSize(1300, 600);
 		frame.setVisible(true);
 	}
 }
