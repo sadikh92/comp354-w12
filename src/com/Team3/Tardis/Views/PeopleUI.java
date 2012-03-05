3f338a33387ba2d1cbbe845a0c0472a5a7efdcf2
constablebumley@yahoo.ca <constablebumley@yahoo.ca@3195375d-7cfc-d6e3-db11-b98cc0a75730>
2012-03-05 18:26:48 +0000

    Added more comments, fixed the method names.

--- src/com/Team3/Tardis/Views/PeopleUI.java
+++ src/com/Team3/Tardis/Views/PeopleUI.java
@@ -1,11 +1,7 @@
 package com.Team3.Tardis.Views;
 
 import com.Team3.Tardis.Models.*;
-import com.Team3.Tardis.Controller.*;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
