9678e64b2214e3210ea2b277917296978fa7979b
rjaffari <rjaffari@3195375d-7cfc-d6e3-db11-b98cc0a75730>
2012-03-06 03:52:11 +0000

    Re-organized utility classes.

--- trunk/src/com/Team3/Tardis/Tests/Models/Tasks/EmptyTasksFile.java
+++ trunk/src/com/Team3/Tardis/Tests/Models/Tasks/EmptyTasksFile.java
@@ -8,8 +8,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.Team3.Tardis.Models.Task;
-import com.Team3.Tardis.Models.XML.InputValidator;
 import com.Team3.Tardis.Models.XML.TaskReader;
+import com.Team3.Tardis.Util.InputValidator;
 
 /*
  * @author Babacar Ndiaye
