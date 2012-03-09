6042f73780fcb29dc62d7145be16ca0711867822
constablebumley@yahoo.ca <constablebumley@yahoo.ca@3195375d-7cfc-d6e3-db11-b98cc0a75730>
2012-03-09 02:08:26 +0000

    Modified to work with the new XML format (super task ID, no indenting).

--- trunk/src/com/Team3/Tardis/Models/XML/TaskWriter.java
+++ trunk/src/com/Team3/Tardis/Models/XML/TaskWriter.java
@@ -41,25 +41,7 @@ public class TaskWriter implements ITasksWriter {
 			sb.append("\n\t\t<deliverable>"+task.getDeliverable()+"</deliverable>");
 			sb.append("\n\t\t<dueDate>"+task.getDueDate()+"</dueDate>");
 			sb.append("\n\t\t<personId>"+task.getPersonId()+"</personId>");
-			
-			/* TODO - subtask handling should be done here for increment 3
-			if(task.getSubtasks() != null && task.getSubtasks().size() > 0) {
-
-				sb.append("\n\t\t<subtasks>");
-					<subtask>
-						<id>0</id>
-						<title>My first sub-task</title>
-						<description>This is my first sub-task...</description>
-						<duration>1</duration>
-						<deliverable>First sub-task.doc</deliverable>
-						<dueDate>01/23/2012</dueDate>
-					</subtask>
-				</subtasks>
-				sb.append("\n\t\t</subtasks>");
-			}
-			else
-				sb.append("\n\t\t<subtasks/>");
-			*/
+			sb.append("\n\t\t<superTaskId>"+task.getSuperTask().getTaskId()+"</superTaskId>");
 				
 			sb.append("\n\t</task>");
 		}
