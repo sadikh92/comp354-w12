3e9e59fb89b16bef7da13f03db3bfbe7ecc79532
constablebumley@yahoo.ca <constablebumley@yahoo.ca@3195375d-7cfc-d6e3-db11-b98cc0a75730>
2012-03-11 16:49:26 +0000

    Updated the test

--- trunk/src/com/Team3/Tardis/Tests/Models/Tasks/TasksLoad.java
+++ trunk/src/com/Team3/Tardis/Tests/Models/Tasks/TasksLoad.java
@@ -11,7 +11,7 @@ import com.Team3.Tardis.Util.InputValidator;
 import com.Team3.Tardis.Util.Logger;
 
 /**
- * @author Bacabar Ndiaye
+ * @author Bacabar Ndiaye, David Campell
  * @description This is a test case to verify that the 
  * 				task reader successfully loads non-empty task files.
  *
@@ -29,7 +29,7 @@ public class TasksLoad {
 		try {
 			tasks = reader.loadTasks(TASKS_FILE);
 			// The file should contain 2 tasks.
-			assertEquals(2, tasks.size());
+			assertEquals(4, tasks.size());
 			
 			// Test the 1st task.
 			
@@ -41,9 +41,7 @@ public class TasksLoad {
 			assertEquals("First task.doc", first.getDeliverable());
 			assertEquals("Mon Jan 23 00:00:00 EST 2012", first.getDueDate().toString());//adapted the test to the date format but my 1st choice was "01/23/2012"
 			assertEquals(2, first.getPersonId());
-			
-
-			// Test the 2nd task.
+			assertEquals(null, first.getSuperTask());
 						
 			Task second = tasks.get(1);
 			assertEquals(1, second.getTaskId());
@@ -53,13 +51,28 @@ public class TasksLoad {
 			assertEquals("Second task.doc", second.getDeliverable());
 			assertEquals("Fri Jan 27 00:00:00 EST 2012", second.getDueDate().toString());
 			assertEquals(1, second.getPersonId());
-
-			//Test the subtasks(subtasks not being read yet)
-			//ArrayList<Task>emptySubTaskList=second.getSubtasks();
-			// The file should contain no subtasks.
-
-			//assertEquals(0, emptySubTaskList.size());
-		    }
+			assertEquals(null, second.getSuperTask());
+			
+			Task third = tasks.get(2);
+			assertEquals(2, third.getTaskId());
+			assertEquals("My first sub-task", third.getTitle());
+			assertEquals("This is my first sub-task...", third.getShortDescription());
+			assertEquals(1, third.getDuration());
+			assertEquals("First sub-task.doc", third.getDeliverable());
+			assertEquals("Mon Jan 23 00:00:00 EST 2012", third.getDueDate().toString());
+			assertEquals(3, third.getPersonId());
+			assertEquals(0, third.getSuperTask().getTaskId());
+			
+			Task fourth = tasks.get(3);
+			assertEquals(3, fourth.getTaskId());
+			assertEquals("My second sub-task", fourth.getTitle());
+			assertEquals("This is my second sub-task...", fourth.getShortDescription());
+			assertEquals(1, fourth.getDuration());
+			assertEquals("Second sub-task.doc", fourth.getDeliverable());
+			assertEquals("Sun Mar 11 00:00:00 EST 2012", fourth.getDueDate().toString());
+			assertEquals(4, fourth.getPersonId());
+			assertEquals(1, fourth.getSuperTask().getTaskId());
+		}
 		
 		catch (Exception e) {
 			// TODO Auto-generated catch block
