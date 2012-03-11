e63fce162d02672f92779d8930c10808562a1b0a
yipksj@gmail.com <yipksj@gmail.com@3195375d-7cfc-d6e3-db11-b98cc0a75730>
2012-03-11 18:22:17 +0000

    test for both delete the giving task with it subtask(s) and delete the subtask with the reference in the super done.

--- src/com/Team3/Tardis/Views/Tests/TaskIUTests.java
+++ src/com/Team3/Tardis/Views/Tests/TaskIUTests.java
@@ -1,5 +1,9 @@
 package com.Team3.Tardis.Views.Tests;
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
+
+import org.junit.Test;
 
 import com.Team3.Tardis.Models.Person;
 import com.Team3.Tardis.Models.Task;
@@ -7,23 +11,126 @@ import com.Team3.Tardis.Models.XML.PeopleReader;
 import com.Team3.Tardis.Models.XML.TaskReader;
 import com.Team3.Tardis.Util.InputValidator;
 import com.Team3.Tardis.Views.TaskEditor;
-import junit.framework.*;
+import com.Team3.Tardis.Views.TaskUI;
 
-/*@author Eric Regnier
- * @description tdb
- */
-public class TaskIUTests extends TestCase {
-
-
+public class TaskIUTests {
 	
-	public TaskIUTests(String name)
+	@Test
+	public void TestDeleteAndSub()
 	{
-		super(name);
+		int index = 0;//the delete index
+		try{
+			//setup
+			InputValidator validator = new InputValidator();
+			PeopleReader peopleReader = new PeopleReader(validator);
+			TaskReader taskReader = new TaskReader(validator);	
+			ArrayList<Person> people = peopleReader.loadPeople(Common.PEOPLE_FILE);
+			ArrayList<Task> tasks = taskReader.loadTasks(Common.TASKS_FILE);
+			
+			TaskUIWrapper testTaskUI = new TaskUIWrapper(new TardisShellMock(), tasks, people);
+			
+			assertTrue(testDelAndSub(tasks, testTaskUI, index));//test
+		}
+		catch (Exception e)
+		{
+		}
+	}
+	@Test
+	public void TestDeleteAndSuper()
+	{
+		int index = 3;//the delete index
+		try{
+			//setup
+			InputValidator validator = new InputValidator();
+			PeopleReader peopleReader = new PeopleReader(validator);
+			TaskReader taskReader = new TaskReader(validator);	
+			ArrayList<Person> people = peopleReader.loadPeople(Common.PEOPLE_FILE);
+			ArrayList<Task> tasks = taskReader.loadTasks(Common.TASKS_FILE);
+			
+			TaskUIWrapper testTaskUI = new TaskUIWrapper(new TardisShellMock(), tasks, people);
+			
+			assertTrue(testDelAndSuper(tasks, testTaskUI, index));//test
+		}
+		catch (Exception e)
+		{
+		}
 	}
 	
-	private void TestDelete()
+	private boolean testDelAndSub(ArrayList<Task> taskList, TaskUIWrapper testTaskUI, int index)
 	{
+		Task temp = testTaskUI.getTasks().get(index);
+		testTaskUI.delect(index);
 		
+		ArrayList<Integer> delIndex = new ArrayList<Integer>();
+		delIndex.add(index);
+		
+		if(!testTaskUI.getTasks().contains(temp))//check for element deleted
+		{
+			for(int i=0; i<temp.getSubtasks().size(); ++i)//check for subtasks deleted
+			{
+				if(!testTaskUI.getTasks().contains(temp.getSubtasks().get(i)))
+				{
+					delIndex.add(taskList.indexOf(temp.getSubtasks().get(i)));//add index of the subtasks in tasks to delIndex
+					continue;
+				}
+				else
+				{
+					return false;//subtasks not deleted
+				}
+			}
+			for(int i=0; i<taskList.size(); ++i)//test for every other element than the deleted one remains in the list 
+			{
+				if(delIndex.contains(i) ||
+						testTaskUI.getTasks().contains(taskList.get(i)))
+				{
+					continue;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+		else
+		{
+			return false;
+		}
+		return true;//delete runs as expected
+	}
+	
+	private boolean testDelAndSuper(ArrayList<Task> taskList, TaskUIWrapper testTaskUI, int index)
+	{
+		Task temp = testTaskUI.getTasks().get(index);
+		testTaskUI.delect(index);
+		
+		if(!testTaskUI.getTasks().contains(temp))//check for element deleted
+		{
+			
+			for(int i=0; i<taskList.size(); ++i)//test for every other element than the deleted one remains in the list 
+			{
+				if(i==index ||
+						testTaskUI.getTasks().contains(taskList.get(i)))
+				{
+					continue;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+		else
+		{
+			return false;
+		}
+		if(temp.getSuperTask()!=null)//check for subtask reference in super task deleted
+		{
+			if(testTaskUI.getTasks().get(testTaskUI.getTasks().indexOf(temp.getSuperTask())).getSubtasks().contains(temp))
+			{
+				return false;//super task reference not deleted
+			}
+		}
+		return true;//delete runs as expected
 	}
 	
 	private void TestAdd()
@@ -47,3 +154,4 @@ public class TaskIUTests extends TestCase {
 		}
 	}
 }
+
