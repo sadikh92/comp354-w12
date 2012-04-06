4c568fe135f23bf373d8afe95d9d3c43b0025d38
constablebumley@yahoo.ca <constablebumley@yahoo.ca@3195375d-7cfc-d6e3-db11-b98cc0a75730>
2012-04-06 02:20:53 +0000

    Updated test.

--- trunk/src/com/Team3/Tardis/Tests/Models/Tasks/TasksLoad.java
+++ trunk/src/com/Team3/Tardis/Tests/Models/Tasks/TasksLoad.java
@@ -28,59 +28,22 @@ public class TasksLoad {
 		Logger.log("Testing", "Tasks");
 		try {
 			tasks = reader.loadTasks(TASKS_FILE);
-			// The file should contain 4 tasks.
-			assertEquals(4, tasks.size());
+			// The file should contain 1 task
+			assertEquals(1, tasks.size());
 			
 			// Test the 1st task.
-			
 			Task first = tasks.get(0);
 			assertEquals(0, first.getTaskId());
 			assertEquals("My first task", first.getTitle());
 			assertEquals("This is my first task...", first.getShortDescription());
 			assertEquals(1, first.getDuration());
 			assertEquals("First task.doc", first.getDeliverable());
+			assertEquals("Mon Jan 16 00:00:00 EST 2012", first.getBeginDate().toString());
 			assertEquals("Mon Jan 23 00:00:00 EST 2012", first.getDueDate().toString());
 			assertEquals(2, first.getPersonId());
 			assertEquals(null, first.getSuperTask());
 			assertEquals(0, first.getCompletionPercentage());
 			assertEquals(null, first.getSuccessor());
-						
-			Task second = tasks.get(1);
-			assertEquals(1, second.getTaskId());
-			assertEquals("My second task", second.getTitle());
-			assertEquals("This is my second task...", second.getShortDescription());
-			assertEquals(3, second.getDuration());
-			assertEquals("Second task.doc", second.getDeliverable());
-			assertEquals("Fri Jan 27 00:00:00 EST 2012", second.getDueDate().toString());
-			assertEquals(1, second.getPersonId());
-			assertEquals(null, second.getSuperTask());
-			assertEquals(0, second.getCompletionPercentage());
-			assertEquals(0, second.getSuccessor().getTaskId());
-			
-			
-			Task third = tasks.get(2);
-			assertEquals(2, third.getTaskId());
-			assertEquals("My first sub-task", third.getTitle());
-			assertEquals("This is my first sub-task...", third.getShortDescription());
-			assertEquals(1, third.getDuration());
-			assertEquals("First sub-task.doc", third.getDeliverable());
-			assertEquals("Mon Jan 23 00:00:00 EST 2012", third.getDueDate().toString());
-			assertEquals(3, third.getPersonId());
-			assertEquals(0, third.getSuperTask().getTaskId());
-			assertEquals(0, third.getCompletionPercentage());
-			assertEquals(0, third.getSuccessor().getTaskId());
-			
-			Task fourth = tasks.get(3);
-			assertEquals(3, fourth.getTaskId());
-			assertEquals("My second sub-task", fourth.getTitle());
-			assertEquals("This is my second sub-task...", fourth.getShortDescription());
-			assertEquals(1, fourth.getDuration());
-			assertEquals("Second sub-task.doc", fourth.getDeliverable());
-			assertEquals("Sun Mar 11 00:00:00 EST 2012", fourth.getDueDate().toString());
-			assertEquals(4, fourth.getPersonId());
-			assertEquals(1, fourth.getSuperTask().getTaskId());
-			assertEquals(0, fourth.getCompletionPercentage());
-			assertEquals(1, fourth.getSuccessor().getTaskId());
 		}
 		
 		catch (Exception e) {
@@ -88,5 +51,4 @@ public class TasksLoad {
 			e.printStackTrace();
 		}
 	}
-
 }
