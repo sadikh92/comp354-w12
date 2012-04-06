e3b3e6a1511f1f42e4c60ae82f23461046a5f698
constablebumley@yahoo.ca <constablebumley@yahoo.ca@3195375d-7cfc-d6e3-db11-b98cc0a75730>
2012-04-06 03:07:49 +0000

    Changed the names of the files for clarity.

--- trunk/src/com/Team3/Tardis/Views/Tests/Common.java
+++ trunk/src/com/Team3/Tardis/Views/Tests/Common.java
@@ -1,20 +1,31 @@
 package com.Team3.Tardis.Views.Tests;
 /**
- * @author Eric Regnier
+ * @author Eric Regnier, David Campbell
  * @description Common variables used for the tests.
- * @Last modified 3/11/12 13:09
+ * @Last modified 5/4/12 10:54
  */
 
 public class Common {
 	
 	static final String PEOPLE_FILE = "xml/people.xml";
 	static final String TASKS_FILE = "xml/tasks.xml";
-	static final String ONE_TASK_TEST_FILE = "report/report_test.txt";
-	static final String ONE_TASK_ACTUAL_FILE = "report/report_one_task.txt";
+	
+	//static final String ONE_TASK_TEST_FILE = "report/report_test.txt";
+	//static final String ONE_TASK_ACTUAL_FILE = "report/report_one_task.txt";
+	static final String ONE_TASK_TEST_FILE = "report/report_one_task_test.txt";
+	static final String ONE_TASK_ACTUAL_FILE = "report/report_one_task_actual.txt";
 	static final String ONE_TASK_TASKS_FILE = "tests/tasks_one_task.xml";
-	static final String NO_TASK_TEST_FILE = "report/report_unassigned.txt";
-	static final String NO_TASK_ACTUAL_FILE = "report/report_no_task.txt";
+	
+	//static final String NO_TASK_TEST_FILE = "report/report_unassigned.txt";
+	//static final String NO_TASK_ACTUAL_FILE = "report/report_no_task.txt";
+	static final String NO_TASK_TEST_FILE = "report/report_unassigned_test.txt";
+	static final String NO_TASK_ACTUAL_FILE = "report/report_unassigned_actual.txt";
 	static final String NO_TASK_TASKS_FILE = "tests/tasks_unassigned.xml";
+	
+	static final String MULTIPLE_TASKS_TEST_FILE = "report/report_multiple_tasks_test.txt";
+	static final String MULTIPLE_TASKS_ACTUAL_FILE = "report/report_multiple_tasks_actual.txt";
+	static final String MULTIPLE_TASKS_FILE = "tests/tasks_multiple_tasks.xml";
+	
 	static final String REPORT_TASK_ACTUAL = "report/report_task_actual.txt";
 	static final String REPORT_TASK_TEST = "report/report_task_test.txt";
 }
