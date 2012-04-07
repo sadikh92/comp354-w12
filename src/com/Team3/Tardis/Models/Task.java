34113a32626c1647059399ce432a06112907719a
constablebumley@yahoo.ca <constablebumley@yahoo.ca@3195375d-7cfc-d6e3-db11-b98cc0a75730>
2012-04-07 04:53:09 +0000

    Cleaned up the code.

--- src/com/Team3/Tardis/Models/Task.java
+++ src/com/Team3/Tardis/Models/Task.java
@@ -6,7 +6,7 @@ import java.util.Date;
  * @author Eric Regnier,Kam Yip,Alex Landovskis,Adam Anderson,Jaffari Rahmatullah,
  * David Campbell,Babacar Ndiaye
  * @Description Task class
- * @Last modified 4/5/12 19:44
+ * @Last modified 7/4/12 12:52
  */
 public class Task {
 
@@ -16,32 +16,19 @@ public class Task {
 	private int duration;
 	private String deliverable;
 	private Date beginDate;
-	private int level;
-	public int getLevel() {
-		return level;
-	}
-
-	public void setLevel(int level) {
-		this.level = level;
-	}
-
-	public Date getBeginDate() {
-		return beginDate;
-	}
-
-	public void setBeginDate(Date beginDate) {
-		this.beginDate = beginDate;
-	}
-
 	private Date dueDate;
 	private int personId;
 	private ArrayList<Task> subTasks;
 	private Task superTask;
+	private int level;
 	private int completionPercentage;
-	//this task must finish before successor to start,a task can  have only ONE successor,
-	//subtasks of the SAME parent can depend on each other but at least for one subtask the successor must be the parent task.
+	//The current task must finish before its successor may start.
+	//Successors can be super tasks, sibling tasks,
+	//or (in the case that the current task has no super task) another task with no super task.
 	private Task successor;	
-	//a task can have more than one predecessors
+	//The current task can only start once its predecessors have finished.
+	//Predecessors can be sub tasks, sibling tasks,
+	//or (in the case that the current task has no super task) another task with no super task.
 	private ArrayList<Task> predecessors;
 	private String status;
 	
@@ -93,6 +80,14 @@ public class Task {
 	public void setDeliverable(String deliverable) {
 		this.deliverable = deliverable;
 	}
+	
+	public Date getBeginDate() {
+		return beginDate;
+	}
+
+	public void setBeginDate(Date beginDate) {
+		this.beginDate = beginDate;
+	}
 
 	public Date getDueDate() {
 		return dueDate;
@@ -126,6 +121,15 @@ public class Task {
 	public void setSuperTask(Task superTask){
 		this.superTask = superTask;
 	}
+	
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	
 	public int getCompletionPercentage() {
 		return completionPercentage;
 	}
@@ -133,6 +137,7 @@ public class Task {
 	public void setCompletionPercentage(int completionPercentage) {
 		this.completionPercentage = completionPercentage;
 	}
+	
 	public Task getSuccessor(){
 		return successor;
 	}
@@ -140,6 +145,7 @@ public class Task {
 	public void setSuccessor(Task successor){
 		this.successor = successor;
 	}
+	
 	public ArrayList<Task> getPredecessors() {
 		return predecessors;
 	}
@@ -147,6 +153,7 @@ public class Task {
 	public void addPredecessor(Task task) {
 		predecessors.add(task);
 	}
+	
 	public String getStatus() {
 		return status;
 	}
@@ -154,52 +161,65 @@ public class Task {
 	public void setStatus(String status) {
 		this.status = status;
 	}
+	
 	//Set status and percentage 
-	public void updateCompletion(){
-	//if task has no predecessors
-	if (this.getPredecessors().isEmpty()&&this.getCompletionPercentage()==0)
+	public void updateCompletion()
 	{
-		this.setStatus("WaitingToRun");
-	}
-	else if(this.getPredecessors().isEmpty()&&this.getCompletionPercentage()==100){
-		this.setStatus("Complete");
-		//other option: maybe delete from list of predecessor of its successor
-	}
-	else if(this.getPredecessors().isEmpty()&&this.getCompletionPercentage()!=0){
-		this.setStatus("In progress");
-	}
-		
-	//if task has predecessors 
-	else if(!this.getPredecessors().isEmpty()){		
-		//compute percentage in predecessor array list
-		int temp=0;
-		for(int i=0;i<this.getPredecessors().size();i++){
-		temp=temp+(this.getPredecessors().get(i).getCompletionPercentage());
+		//if task has no predecessors
+		if (this.getPredecessors().isEmpty() && this.getCompletionPercentage() == 0)
+		{
+			this.setStatus("WaitingToRun");
 		}
-		int result=temp/(this.getPredecessors().size());
-		
-		//if all predecessors are completed,task is ready to start
-		if(result==100&&this.getCompletionPercentage()==0)
-		this.setStatus("WaitingToRun");	
-		
-		//if all predecessors are completed and task ran to completion
-		else if(result==100&&this.getCompletionPercentage()==100)
-		this.setStatus("Complete");	
-		
-		//if all predecessors are completed and task is running
-		else if(result==100&&this.getCompletionPercentage()!=0)
-		this.setStatus("In progress");
-		
-		//if predecessors are not completed yet
-		else if (result!=100){
-		this.setStatus("WaitingForPredecessorToComplete");
-		//reset to 0 if user tries to set completionpercentage for a task before all its predecessors are complete.
-		this.setCompletionPercentage(0);
+		else if (this.getPredecessors().isEmpty() && this.getCompletionPercentage() == 100)
+		{
+			this.setStatus("Complete");
+			//other option: maybe delete from list of predecessor of its successor
 		}
-	}
-					
-	else {
-		this.setStatus("Unknown/Invalid Status");
-	}
+		else if (this.getPredecessors().isEmpty() && this.getCompletionPercentage() != 0)
+		{
+			this.setStatus("In progress");
+		}
+		//if task has predecessors 
+		else if (!this.getPredecessors().isEmpty())
+		{		
+			//compute percentage in predecessor array list
+			int temp = 0;
+			
+			for(int i = 0; i < this.getPredecessors().size() ; i++)
+			{
+				temp = temp + (this.getPredecessors().get(i).getCompletionPercentage());
+			}
+			
+			int result = temp/(this.getPredecessors().size());
+			
+			//if all predecessors are completed,task is ready to start
+			if (result == 100 && this.getCompletionPercentage() == 0)
+			{
+				this.setStatus("WaitingToRun");
+			}
+			//if all predecessors are completed and task ran to completion
+			else if (result == 100 && this.getCompletionPercentage() == 100)
+			{
+				this.setStatus("Complete");
+			}
+			//if all predecessors are completed and task is running
+			else if (result == 100 && this.getCompletionPercentage() != 0)
+			{
+				this.setStatus("In progress");
+			}
+			//if predecessors are not completed yet
+			else if (result!=100)
+			{
+				this.setStatus("WaitingForPredecessorToComplete");
+				
+				//reset to 0 if user tries to set completion percentage for a task
+				//before all its predecessors are complete.
+				this.setCompletionPercentage(0);
+			}
+		}			
+		else
+		{
+			this.setStatus("Unknown/Invalid Status");
+		}
 	}
 }
